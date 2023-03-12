85761748f8b2fd79b26f1458b6d9f832670ff512
unknown <serhiiromanchuk@gmail.com>
2023-03-12 16:24:55 +0200

    add packages

---
+++ app/src/main/java/com/example/myshopinglist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myshopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myshopinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
