0bcc602917444ae5958b059202c9a51da3d27ccb
unknown <serhiiromanchuk@gmail.com>
2023-04-26 18:00:37 +0300

    Use UriMatcher

--- app/src/main/java/com/example/myshopinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/example/myshopinglist/data/ShopListProvider.kt
@@ -2,11 +2,18 @@ package com.example.myshopinglist.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider : ContentProvider() {
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.example.myshopinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+        addURI("com.example.myshopinglist", "shop_items/#", GET_SHOP_ITEM_BY_ID_QUERY)
+    }
+
     override fun onCreate(): Boolean {
         return true
     }
@@ -18,7 +25,16 @@ class ShopListProvider : ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        Log.d("ShopListProvider", "query $uri")
+        val code = uriMatcher.match(uri)
+        when (code) {
+            GET_SHOP_ITEMS_QUERY -> {
+
+            }
+            GET_SHOP_ITEM_BY_ID_QUERY -> {
+
+            }
+        }
+        Log.d("ShopListProvider", "query $uri code $code")
         return null
     }
 
@@ -41,5 +57,10 @@ class ShopListProvider : ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val GET_SHOP_ITEMS_QUERY = 100
+        private const val GET_SHOP_ITEM_BY_ID_QUERY = 101
     }
 }
